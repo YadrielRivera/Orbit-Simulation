94d07dbd37aaedafe68ed3ff643c6fd756e3c870
yadriel.rivera <yadriel.rivera@upr.edu>
2026-05-19 10:55:46 -0400

    rendered window

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,39 @@
 using namespace std;
 
 int main(){
-    cout<<"hello"<< endl;
+    
+    glfwInit();
+    glfwDefaultWindowHints();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+    GLFWwindow* window = glfwCreateWindow(800,600, "Orbit Simulation", NULL, NULL);
+    
+    if(window == NULL){
+
+        cout << "Failed to create window. Terminating process..." << endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+
+        cout << "Failed to initialize GLAD. Terminating process..." << endl;
+        return -1;
+    }
+
+    glViewport(0,0,800,600);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+
+    glfwTerminate();
     return 0;
 }
