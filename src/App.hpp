761c04cc6864f788f9bbd8df71b1f03ebc82d35b
yadriel.rivera <yadriel.rivera@upr.edu>
2026-07-24 21:14:54 -0700

    Added image rendering with stbi for the basic background

--- src/App.hpp
+++ src/App.hpp
@@ -5,4 +5,6 @@
 #include "../include/GLFW/glfw3.h"
 #include "CelestialBodies/Planets/headers/Earth.hpp"
 #include "CelestialBodies/Stars/headers/Sun.hpp"
+
+#define STB_IMAGE_IMPLEMENTATION
 #include "../include/stb_image/stb_image.h"
