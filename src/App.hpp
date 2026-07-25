04f018a981c4324628f99b0f5ce748cdc58a837e
yadriel.rivera <yadriel.rivera@upr.edu>
2026-07-24 20:44:52 -0700

    Fixed relative paths to prevent include path errors

--- src/App.hpp
+++ src/App.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
 #include <iostream>
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
+#include "../include/glad/glad.h"
+#include "../include/GLFW/glfw3.h"
 #include "CelestialBodies/Planets/headers/Earth.hpp"
 #include "CelestialBodies/Stars/headers/Sun.hpp"
+#include "../include/stb_image/stb_image.h"
